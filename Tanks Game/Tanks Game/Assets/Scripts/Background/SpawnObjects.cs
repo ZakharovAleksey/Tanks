46334e1d200399d70495b8b061c08b931424c580
Aleksey <lehazakharov@yandex.ru>
2017-01-18 20:12:55 +0300

    Try to add Current version of the project

---
+++ Tanks Game/Tanks Game/Assets/Scripts/Background/SpawnObjects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public GameObject m_TurretPrefab;
+    public Transform[] m_TurretSpawnPoints;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        SpawnTurrets();
+	}
+
+    private void SpawnTurrets()
+    {
+        for (int i = 0; i < m_TurretSpawnPoints.Length; ++i)
+        {
+            Instantiate(m_TurretPrefab, m_TurretSpawnPoints[i].position, Quaternion.identity);
+        }
+    }
+
+
+}
